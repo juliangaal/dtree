ba2434f9182e0816f8abec1c6c12e79e6572967e
juliangaal <gjulian@uos.de>
2018-07-01 15:34:03 +0200

    box instead of circle style graphviz

--- src/GraphElements.cpp
+++ src/GraphElements.cpp
@@ -18,18 +18,12 @@ string GraphElement::box(const string &label) {
   return box + node(label);
 }
 
-string GraphElement::leaf(const unordered_map<string, int>& data) {
-  return result(data) + " [style=filled, color=\"1 0.8 1\"];\n";
-}
-
-string GraphElement::result(const unordered_map<string, int>& data) {
+string GraphElement::result(const unordered_map<string, int> &data, branch b) {
   static int id = 0;
-  string result = "\"";// + std::to_string(id++) + "\\nvalues: ";
+  string result = "\"";
   double sum = Helper::tree::mapValueSum(data);
   size_t map_size = data.size();
   size_t counter = 1;
-
-  result += "\\n";
 
   for (const auto& [k, v]: data) {
     result += k + ": " + std::to_string(static_cast<int>(v / sum * 100)) + "% ";
@@ -45,6 +39,7 @@ string GraphElement::result(const unordered_map<string, int>& data) {
   }
 
   result += "\\nid: " + std::to_string(id++);
-
-  return result += "\"";
+  result += "\"" + (b ? GraphElement::label("True") : GraphElement::label("False"));
+  result += "   " + result + "\" [shape=box];\n";
+  return result;
 }
