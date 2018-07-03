6445ee7bfb77a38381049498450d8c1c3805000e
juliangaal <gjulian@uos.de>
2018-07-03 15:37:47 +0200

    correct path of file

--- test/import_test.cpp
+++ test/import_test.cpp
@@ -6,8 +6,8 @@
 
 int main(void) {
   Dataset d;
-  d.train.filename = "data/play_tennis.csv";
-  d.test.filename = "data/play_tennis_test.csv";
+  d.train.filename = "../data/play_tennis.csv";
+  d.test.filename = "../data/play_tennis_test.csv";
 
   DecisionTree dt(d);
   return 0;
