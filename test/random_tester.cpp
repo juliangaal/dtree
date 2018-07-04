5fbe05f199d8c4e2988e01d432aa9e0bbcaa810a
juliangaal <gjulian@uos.de>
2018-07-04 14:56:17 +0200

    make random_tester independent from library for quicker dev

--- test/random_tester.cpp
+++ test/random_tester.cpp
@@ -1,9 +1,9 @@
-#include <DecisionTree/DecisionTree.hpp>
+#include "../lib/include/DecisionTree.hpp"
 
 int main() {
   Dataset d;
-  d.train.filename = "data/san_fran_vs_nyc.csv";
-  d.test.filename = "data/san_fran_vs_nyc_test.csv";
+  d.train.filename = "../data/san_fran_vs_nyc.csv";
+  d.test.filename = "../data/san_fran_vs_nyc_test.csv";
 
   DecisionTree dt(d);
   dt.print();
