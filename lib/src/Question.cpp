b5d7cbc4f1f3c6ab5e60f39082597653b2c48924
juliangaal <gjulian@uos.de>
2018-07-05 16:11:10 +0200

    const return types Question

--- lib/src/Question.cpp
+++ lib/src/Question.cpp
@@ -9,7 +9,7 @@ Question::Question() : column_(0), value_("") {}
 
 Question::Question(const int column, const string value) : column_(column), value_(value) {}
 
-bool Question::match(VecS example) const {
+const bool Question::match(VecS example) const {
   const string& val = example[column_];
   if (isNumeric(val)) {
     return std::stod(val) >= std::stod(value_);
@@ -17,14 +17,14 @@ bool Question::match(VecS example) const {
     return val == value_;
   }
 }
-string Question::toString(const VecS& labels) const {
+const string Question::toString(const VecS& labels) const {
   string condition = "==";
   if (isNumeric(value_))
     condition = ">=";
   return "Is " + labels[column_] + " " + condition + " " + value_ + "?";
 }
 
-bool Question::isNumeric(std::string value) const {
+const bool Question::isNumeric(std::string value) const {
   double parsed_number = 0.0;
 
   if (bool empty = value.empty(); !empty) {
